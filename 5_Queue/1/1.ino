c85f4d64cbc5757c4ff2f7246ee63bbfb3a1f3dd
zakwanzamri17 <129892253+zakwanzamri17@users.noreply.github.com>
2023-05-08 07:04:07 +0800

    Add files via upload

---
+++ 5_Queue/1/1.ino
@@ -0,0 +1,50 @@
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+// Settings
+static const uint8_t msg_queue_len = 5;
+
+static QueueHandle_t msg_queue;
+
+
+//Tasks
+
+void printMessages(void *parameters) {
+  int item;
+
+  while (1) {
+    if (xQueueReceive(msg_queue, (void *)&item, 0) == pdTRUE) { 
+      Serial.println(item);
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS Queue Demo---");
+
+  msg_queue = xQueueCreate(msg_queue_len, sizeof(int));
+
+  xTaskCreatePinnedToCore(printMessages,
+                          "Print Messages",
+                          1024,NULL,
+                          1,
+                          NULL,
+                          app_cpu);
+}
+
+void loop() {
+  static int num = 0;
+  if (xQueueSend(msg_queue, (void *)&num, 10) != pdTRUE) {
+    Serial.println("Queue full");
+  }
+  num++;
+
+  vTaskDelay(500 / portTIVK_PERIOD-MS);
+}
